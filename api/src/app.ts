bf43b4cf662eb17c3b339418e3a389fe1500bc87
EmirDelicR <emirdelictbf@gmail.com>
2023-12-09 10:04:40 +0100

    feat: added express server and types

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,3 +1,28 @@
-const x: string = 'data';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import helmet from 'helmet';
 
-console.log(x);
+import path from 'path';
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ debug: true });
+}
+
+const PORT: number = parseInt(process.env.PORT!, 10) || 3100;
+const HOST: string = process.env.HOST! || 'localhost';
+
+const app = express();
+
+app.use('/static', express.static(path.join(__dirname, 'images')));
+
+app.use(cors());
+
+app.use(helmet());
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, HOST, () => {
+  console.info(`Server running at http://${HOST}:${PORT}/`);
+});
