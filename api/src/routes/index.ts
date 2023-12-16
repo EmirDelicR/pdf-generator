20a1773ba148d2d83faa5eaaf2288f823dcbeda7
EmirDelicR <emirdelictbf@gmail.com>
2023-12-16 09:28:04 +0100

    feat: Added property validation

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,7 +1,5 @@
 import { Application } from 'express';
 
-import { middleware } from 'src/middleware';
-
 const registerRoutes = (server: Application) => {
   server.get('/', (_req, res) => {
     res.send('Hello from server');
