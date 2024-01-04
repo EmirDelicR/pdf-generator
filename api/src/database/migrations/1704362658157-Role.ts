094e3dd51331bb8580cec7007f6ff7ec58248319
EmirDelicR <emirdelictbf@gmail.com>
2024-01-04 12:28:32 +0100

    feat: update migration

--- api/src/database/migrations/1704362658157-Role.ts
+++ api/src/database/migrations/1704362658157-Role.ts
@@ -1,11 +1,31 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
-
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { DBTableNames } from 'src/utils/constants/db';
 export class Role1704362658157 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: DBTableNames.ROLE,
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            isUnique: true,
+            generationStrategy: 'uuid'
+          },
+          {
+            name: 'type',
+            type: 'number',
+            isNullable: false,
+            isUnique: true
+          }
+        ]
+      }),
+      true
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(DBTableNames.ROLE);
+  }
 }
